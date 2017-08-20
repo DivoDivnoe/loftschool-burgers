daf388ed65824691166ea4b77485aae160ba9d9e
DivoDivnoe <andrey-tm203@mail.ru>
2017-08-21 02:35:23 +0300

    implement popup interactivity in slider section

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ $(() => {
   const fullScreenMenu = $('.fullscreen-menu');
   const fullScreenMenuCloser = fullScreenMenu.find('.hamburger-menu__close');
 
-  /*const structure = $('.slider__structure');
-  const structureTable = structure.find('.structure__table');
-  const structureTableCloser = structureTable.find('.structure__close');*/
+  const wrapper = $('.slider__structure-wrapper');
+  const structure = wrapper.find('.slider__structure');
+  const structureTable = wrapper.find('.structure__table');
+  const structureTableCloser = structureTable.find('.structure__close');
 
   hamburger.on('click', () => {
     fullScreenMenu.fadeIn(300);
@@ -14,4 +15,14 @@ $(() => {
     evt.preventDefault();
     fullScreenMenu.fadeOut(300);
   });
+
+  if (window.matchMedia('screen and (max-width: 768px)').matches) {
+    structure.on('click', () => {
+      structureTable.toggleClass('structure__table--active');
+    });
+    structureTableCloser.on('click', (evt) => {
+      evt.preventDefault();
+      structureTable.removeClass('structure__table--active');
+    });
+  }
 })
